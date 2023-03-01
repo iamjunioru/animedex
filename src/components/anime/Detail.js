a95a121f4a9a12b36c898b122bb704ec266e06c6
Junior <72615418+iamjunioru@users.noreply.github.com>
2023-03-01 03:09:29 -0300

    adicionado um tratamento p a API caso não retorne nenhum dado.
    tbm foi removido a checagem desnecessária do img e adicionou a verificação na render do componente

--- src/components/anime/Detail.js
+++ src/components/anime/Detail.js
@@ -10,70 +10,46 @@ class Detail extends Component {
 
   componentDidMount() {
     axios
-    .get(`https://kitsu.io/api/edge/anime?filter[id]=${this.props.match.params.id}`)
-    .then(res => {
-      this.setState({
-        detail: res.data.data[0].attributes,
-        img: res.data.data[0].attributes.coverImage
-      });
-    });  
+      .get(`https://kitsu.io/api/edge/anime?filter[id]=${this.props.match.params.id}`)
+      .then(res => {
+        const data = res.data.data[0];
+        if (data) {
+          this.setState({
+            detail: data.attributes,
+            img: data.attributes.coverImage
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
-    const { detail } = this.state;
-    const { img } = this.state;
+    const { detail, img } = this.state;
 
-    if (
-      detail === undefined ||
-      img === undefined ||
-      Object.keys(detail).length === 0
-    ) {
+    if (!detail || Object.keys(detail).length === 0) {
       return <Spinner />;
-    } else {
-      if (img !== null) {
-        return (
-          <div id="detail" className="card mb-4">
-            <img
-              className="card-img-top"
-              id="pic-title"
-              src={img.large}
-              alt="Card cap"
-            />
-            <div className="card-body">
-              <h2 className="card-title">
-                {detail.canonicalTitle}
-              </h2>
-              <p className="card-text">{detail.synopsis}</p>
-              <h4 className="card-text">
-                <b>Avaliação</b>: {detail.averageRating}/100 <br />
-                <b>Rank de Popularidade</b> : {detail.popularityRank}
-              </h4>
-            </div>
-          </div>
-        );
-      } else {
-        return (
-          <div id="detail" className="card mb-3">
-            <img
-              className="card-img-top"
-              id="pic-title"
-              src="https://www.pngmart.com/files/22/Kurama-PNG-Isolated-Image.png"
-              alt="Card cap"
-            />
-            <div className="card-body">
-              <h3 className="card-title text-center mb-4">
-                {detail.canonicalTitle}
-              </h3>
-              <p className="card-text">{detail.synopsis}</p>
-              <p className="card-text">
-                <b>Avaliação</b>: {detail.averageRating}/100 <br />
-                <b>Rank de popularidade</b> : {detail.popularityRank}
-              </p>
-            </div>
-          </div>
-        );
-      }
     }
+
+    return (
+      <div id="detail" className="card mb-4">
+        {img && (
+          <img
+            className="card-img-top"
+            id="pic-title"
+            src={img.large}
+            alt="Card cap"
+          />
+        )}
+        <div className="card-body">
+          <h2 className="card-title">{detail.canonicalTitle}</h2>
+          <p className="card-text">{detail.synopsis}</p>
+          <h4 className="card-text">
+            <b>Avaliação</b>: {detail.averageRating}/100 <br />
+            <b>Rank de Popularidade</b> : {detail.popularityRank}
+          </h4>
+        </div>
+      </div>
+    );
   }
 }
 
