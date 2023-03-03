a2146fc1a8b71090921e0b6fb22ff0be0de3b800
Junior <72615418+iamjunioru@users.noreply.github.com>
2023-03-03 05:38:36 -0300

    footer add

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <footer>
-      test eae
+    <footer className='footerzim'>
+      Um projeto final feito para a disciplina de PWEB2.<br></br>
+      <a href className='gitlink'>Github - LinkedIn</a>
+      
     </footer>
   );
 }
