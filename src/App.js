e69b279cb1aa7c443af02ed10b0be833e2daae20
Junior <72615418+iamjunioru@users.noreply.github.com>
2023-03-07 14:47:45 -0300

    update footer

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ const App = () => {
             <Route exact path="/" component={Index} />
             <Route exact path="/sobre" component={About} />
             <Route exact path="/anime/detalhes/:id" component={Detail} />
-            <Route exact path="/2" render={() => <h1>Lorem ipsum dolor sit amet</h1>} />
-            <Route exact path="/3" render={() => <h1>Consectetur adipiscing elit</h1>} />
+            <Route exact path="/2" render={() => <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions.</h1>} />
+            <Route exact path="/3" render={() => <h1>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</h1>} />
             <Redirect to="/" />
           </Switch>
           <Footer />
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,9 +4,8 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className='footerzim'>
-      Um projeto final feito para a disciplina de PWEB2.<br></br>
-      <a href className='gitlink'>Github - LinkedIn</a>
-      
+      Animedex project to PWEB2.<br></br>
+      Issues? Repository on <a href="https://www.github.com/iamjunioru/animedex" target="_blank" className='gitlink'>Github</a>.  
     </footer>
   );
 }
